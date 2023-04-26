0925d266c3764b326af3a74eb09cb7942c98ffed
rob$$ <42154949+rhicksiii91@users.noreply.github.com>
2023-04-26 10:38:53 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_acl" "exposedbucket_acl" {
 
 resource "aws_s3_bucket_public_access_block" "private" {
   bucket                  = aws_s3_bucket.exposedbucket.id
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
